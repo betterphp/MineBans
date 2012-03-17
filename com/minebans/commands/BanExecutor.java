a5c3b076f17ecce5efa6f70317de4b2d0292d8a0
Jacek <jacek@kuzemczak.co.uk>
2012-03-17 02:53:35 +0000

    v0.3.2 If a player has been seen before is now correctly checked again

--- com/minebans/commands/BanExecutor.java
+++ com/minebans/commands/BanExecutor.java
@@ -70,7 +70,7 @@ public class BanExecutor implements CommandExecutor {
 			}else{
 				OfflinePlayer player = plugin.server.getOfflinePlayer(playerName);
 				
-				if (player.isOnline() == false && player.hasPlayedBefore() == false && player.getLastPlayed() != 0L && player.getFirstPlayed() != 0L){
+				if (player.isOnline() == false && (player.hasPlayedBefore() == false || player.getLastPlayed() == 0L || player.getFirstPlayed() == 0L)){
 					sender.sendMessage(plugin.formatMessage(ChatColor.RED + "You cannot globally ban a player that has never connected to the server."));
 					return true;
 				}
