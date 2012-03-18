6a82c7f85d88c7286a24c59840aa6dee12fb15f5
Jacek <jacek@kuzemczak.co.uk>
2012-03-18 14:57:16 +0000

    v0.4 changed /whitelist to /exempt

--- com/minebans/commands/ExemptExecutor.java
+++ com/minebans/commands/ExemptExecutor.java
@@ -8,22 +8,22 @@ import org.bukkit.entity.Player;
 
 import com.minebans.MineBans;
 
-public class WhiteListExecutor implements CommandExecutor {
+public class ExemptExecutor implements CommandExecutor {
 	
 	private MineBans plugin;
 	
-	public WhiteListExecutor(MineBans plugin){
+	public ExemptExecutor(MineBans plugin){
 		this.plugin = plugin;
 	}
 	
 	public boolean onCommand(CommandSender sender, Command command, String string, String[] args){
-		if (sender.hasPermission("minebans.admin.whitelist") == false){
+		if (sender.hasPermission("minebans.admin.exempt") == false){
 			sender.sendMessage(plugin.formatMessage(ChatColor.RED + "You do not have permission to use this command."));
 			return true;
 		}
 		
 		if (args.length != 2){
-			sender.sendMessage(plugin.formatMessage(ChatColor.RED + "Usage: /whitelist <option> <player_name>"));
+			sender.sendMessage(plugin.formatMessage(ChatColor.RED + "Usage: /exempt <option> <player_name>"));
 			sender.sendMessage(plugin.formatMessage(ChatColor.RED + "Options:"));
 			sender.sendMessage(plugin.formatMessage(ChatColor.RED + "   add - Adds the player to the whitelist."));
 			sender.sendMessage(plugin.formatMessage(ChatColor.RED + "   remove - Removed the player from the whitelist."));
@@ -34,23 +34,23 @@ public class WhiteListExecutor implements CommandExecutor {
 		String playerName = args[1];
 		
 		if (option.equalsIgnoreCase("add") || option.equalsIgnoreCase("a")){
-			if (plugin.banManager.isWhitelisted(playerName)){
-				sender.sendMessage(plugin.formatMessage(ChatColor.RED + playerName + " is already on the whitelist."));
+			if (plugin.banManager.isExempt(playerName)){
+				sender.sendMessage(plugin.formatMessage(ChatColor.RED + playerName + " is already on the exempt list."));
 				return true;
 			}
 			
-			plugin.banManager.whiteListPlayer(playerName, (sender instanceof Player));
+			plugin.banManager.exemptPlayer(playerName, (sender instanceof Player));
 			
-			sender.sendMessage(plugin.formatMessage(ChatColor.GREEN + playerName + " has been added to the whitelist."));
+			sender.sendMessage(plugin.formatMessage(ChatColor.GREEN + playerName + " has been added to the exempt list."));
 		}else if (option.equalsIgnoreCase("remove") || option.equalsIgnoreCase("r")){
-			if (plugin.banManager.isWhitelisted(playerName) == false){
-				sender.sendMessage(plugin.formatMessage(ChatColor.RED + playerName + " is not on the whitelist."));
+			if (plugin.banManager.isExempt(playerName) == false){
+				sender.sendMessage(plugin.formatMessage(ChatColor.RED + playerName + " is not on the exempt list."));
 				return true;
 			}
 			
-			plugin.banManager.unWhiteListPlayer(playerName, (sender instanceof Player));
+			plugin.banManager.unExemptPlayer(playerName, (sender instanceof Player));
 			
-			sender.sendMessage(plugin.formatMessage(ChatColor.GREEN + playerName + " has been removed from the whitelist."));
+			sender.sendMessage(plugin.formatMessage(ChatColor.GREEN + playerName + " has been removed from the exempt list."));
 		}else{
 			sender.sendMessage(plugin.formatMessage(ChatColor.RED + "Invalid option, see /whitelist for a list of options."));
 			return true;
