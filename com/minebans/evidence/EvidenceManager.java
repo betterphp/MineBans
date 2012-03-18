d4ff57bd5ee71a2c8b89cfc3e0dc2170a1c4ed63
Jacek <jacek@kuzemczak.co.uk>
2012-03-18 03:44:24 +0000

    v0.3.3 Evidence for all rules is now collected properly

---
+++ com/minebans/evidence/EvidenceManager.java
@@ -0,0 +1,30 @@
+package com.minebans.evidence;
+
+import java.util.HashMap;
+
+import com.minebans.MineBans;
+import com.minebans.bans.BanReason;
+
+public class EvidenceManager {
+	
+	private HashMap<BanReason, EvidenceCollector> collectorMap;
+	
+	public EvidenceManager(MineBans plugin){
+		this.collectorMap = new HashMap<BanReason, EvidenceCollector>();
+		
+		for (BanReason reason : BanReason.getAll()){
+			if (plugin.config.getBoolean("max-bans." + reason.getConfigKey() + ".enabled")){
+				this.collectorMap.put(reason, reason.getEvidenceCollector());
+			}
+		}
+	}
+	
+	public Object collectFor(BanReason reason, String playerName){
+		if (this.collectorMap.containsKey(reason) == false){
+			return null;
+		}
+		
+		return this.collectorMap.get(reason).collect(playerName);
+	}
+	
+}
